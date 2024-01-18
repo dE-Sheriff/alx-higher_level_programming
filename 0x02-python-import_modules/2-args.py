2f633619fd4fadc9aee97db1a94d4fb4f6dfe4f3
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-18 14:49:10 +0100

    edit on all

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if "__main__" == __name__:
+    import sys
+    arg_len = len(sys.argv) - 1
+    if arg_len == 1:
+        print(f"{arg_len} argument:")
+    else:
+        print(f"{arg_len} arguments:")
+    for index in range(arg_len):
+        print(f"{index + 1}: {sys.argv[index + 1]}")
