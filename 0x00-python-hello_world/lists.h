49b83780750bb77741a2f08232ac4b35059802e6
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-18 07:33:27 +0100

    after merge

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -11,7 +11,6 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -22,6 +21,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-
 
 #endif /* LISTS_H */
