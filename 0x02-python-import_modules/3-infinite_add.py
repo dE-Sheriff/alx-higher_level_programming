11fd4a8050a8772b0013efa7ccffe5833a16ad0b
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-18 15:06:50 +0100

    edit on 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if "__main__" == __name__:
+    import sys
+    total = 0
+    arg_len = len(sys.argv) - 1
+    
+    for num in range(arg_len):
+        total += int(sys.argv[num + 1])
+    print(total)
