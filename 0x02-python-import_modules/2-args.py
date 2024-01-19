ae9c2ffc026e91b648339a4cfe0c21fe22fd202b
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-19 07:15:15 +0100

    edit on 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if "__main__" == __name__:
     import sys
     arg_len = len(sys.argv) - 1
     if arg_len == 1:
-        print(f"{arg_len} argument.")
+        print(f"{arg_len} argument:")
+    elif arg_len == 0:
+        print(f"{arg_len} arguments.")
     else:
         print(f"{arg_len} arguments:")
     for index in range(arg_len):
