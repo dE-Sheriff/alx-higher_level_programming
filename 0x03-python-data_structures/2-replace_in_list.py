22169361f0a69973c3354ac51c65e94043fbbfcd
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-19 21:08:54 +0100

    add on data struct 34

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,6 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
-    my_list.replace(my_list[idx])
-    my_list.insert(idx, element)
+    list_copy = my_list
+    list_copy[idx] = element
+    return list_copy
