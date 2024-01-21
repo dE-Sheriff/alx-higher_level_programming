9a546299fa5c64a6bf2458415fca165040a94f4a
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-21 14:36:07 +0100

    edit on 8 and 9 Dstruct

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str_len = len(sentence)
-    first_chr = sentence[0]
     if (sentence == ""):
         first_chr = None
+        str_len = 0
+    else:
+        str_len = len(sentence)
+        first_chr = sentence[0]
     return (str_len, first_chr)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     biggest_int = 0
     for num in my_list:
+        if len(my_list) == 0:
+            return None
         if num > biggest_int:
             biggest_int = num
     return biggest_int
