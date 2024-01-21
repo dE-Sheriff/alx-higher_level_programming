6add3441ae850e1bcf36227915cb78f1636384e4
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-21 14:44:11 +0100

    edit on 9 Dstruct

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest_int = 0
-    for num in my_list:
-        if len(my_list) == 0:
-            return None
-        if num > biggest_int:
-            biggest_int = num
-    return biggest_int
+    if len(my_list) == 0 or my_list is None:
+        return None
+    h = list(reversed(sorted(my_list)))[0]
+    return h
