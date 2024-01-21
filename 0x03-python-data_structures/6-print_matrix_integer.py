fa04e8292895d2973997a63052d6999c2ffba8ba
Sarafadeen Sheriff <sheriffsarafa@gmail.com>
2024-01-21 08:21:59 +0100

    add on data struc 67

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        line = []
+        for col in row:
+            line.append("{:d}".format(col))
+        print(" ".join(line))
