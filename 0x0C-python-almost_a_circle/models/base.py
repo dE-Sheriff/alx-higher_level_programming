4d3d0df56b6c7a2743d666561cd358d0e09510d8
Sheriff Sarafa <sheriffsarafa@gmail.com>
2024-02-27 03:05:13 +0100

    add to py almot a Cir 00 - 01

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+"""
+Base model for all tasks
+"""
+
+class Base:
+    """
+    The Base Model
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Initializing the Base class for any object with id
+        id: id of the Base oject
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
