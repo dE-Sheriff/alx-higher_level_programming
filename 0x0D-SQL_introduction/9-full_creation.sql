c0b1da8eaf83b0bddfa2f53eaa33c8dc13d829af
Sheriff Sarafa <sheriffsarafa@gmail.com>
2024-03-30 14:26:38 +0100

    edit on intro SQL 09

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `second_table` (
 );
 
 INSERT INTO `second_table` (`id`, `name`, `score`)
-VALUE (1, “John”, 10),
-(2, “Alex”, 3),
-(3, “Bob”, 14),
-(4, “George”, 8);
+VALUE (1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
