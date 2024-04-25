0ef7a34e56ae20d705c69f628fd690e05512f74f
Sheriff Sarafa <sheriffsarafa@gmail.com>
2024-04-25 04:29:31 +0100

    add to JS warm up

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArgs = process.argv.slice(2);
+if (myArgs.length <= 1) {
+	console.log(0);
+} else {
+	console.log(myArgs.map((item) => Number(item)).sort((a, b) => a - b).reverse()[1]);
+}
