bf4a0da27f1810b6e2d94039addd6f67dcc56a40
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-07-06 15:13:40 +0100

    edit on 14 ORM

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""list all States from the database hbtn_0e_6_usa"""
+"""Definition of the City class"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+# from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
-def main():
-    """connect to db, read data and display as expected using ORM"""
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            username, password, database), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class City(Base):
+    """Class City"""
 
-    session = Session(engine)
-    for state in session.query(State).filter(State.name.like('%a%')):
-        session.delete(state)
-    session.commit()
-    session.flush()
-    session.close()
-
-
-if __name__ == '__main__':
-    """execute the main function"""
-    main()
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
